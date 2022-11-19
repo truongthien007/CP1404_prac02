5f2f5f270c46f458a6e7217a7d3bf99ca12c0e02
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-20 01:02:58 +0700

    change the way print result

--- password_stars.py
+++ password_stars.py
@@ -3,5 +3,4 @@ name = input("Enter a name with at least 10 characters: ")
 while len(name) < length:
     print("invalid name")
     name = input("Enter a name with at least 10 characters: ")
-for i in range(len(name)):
-    print("*" * len(name), end=" ")
+print("*" * len(name), end=" ")
