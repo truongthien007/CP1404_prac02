d714d6574231cdac9996ad256e6dcdb75527da56
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-20 01:10:31 +0700

    change code by using function

--- password_stars.py
+++ password_stars.py
@@ -1,6 +1,24 @@
+# length = 10
+# name = input("Enter a name with at least 10 characters: ")
+# while len(name) < length:
+#     print("invalid name")
+#     name = input("Enter a name with at least 10 characters: ")
+# print("*" * len(name), end=" ")
+
 length = 10
-name = input("Enter a name with at least 10 characters: ")
-while len(name) < length:
-    print("invalid name")
+
+
+def main():
+    name = validate_name()
+    print("*" * len(name), end=" ")
+
+
+def validate_name():
     name = input("Enter a name with at least 10 characters: ")
-print("*" * len(name), end=" ")
+    while len(name) < length:
+        print("invalid name")
+        name = input('Enter a name with at least 10 characters: ')
+    return name
+
+
+main()
