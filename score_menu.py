86607dabaf061d290f6a8efaf56be4d9e33231d9
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-20 16:26:42 +0700

    add some comment

--- score_menu.py
+++ score_menu.py
@@ -8,12 +8,15 @@ def main():
     choice = input("Your choice: ").upper()
     while choice != "Q":
         if choice == "G":
+            """to get valid score"""
             validate_score()
         elif choice == "P":
+            """print the result"""
             final_score = validate_score()
             result = determine_score(final_score)
             print(result)
         elif choice == "S":
+            "print as many stars as score"
             final_score = validate_score()
             print("*" * final_score)
         else:
@@ -24,6 +27,7 @@ def main():
 
 
 def validate_score():
+    """check valid score"""
     score = int(input("Enter score: "))
     while score < 0 or score > 100:
         print("Invalid score")
@@ -32,6 +36,7 @@ def validate_score():
 
 
 def determine_score(final_score):
+    """determine the result"""
     if final_score < 50:
         result = "Bad"
     elif final_score < 90:
